ec54519c2acde566fbb4b3784bef7b9880496d67
randall.erasmus <randallerasmus1@gmail.com>
2020-12-01 22:49:56 +0200

    Add routing for components

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -2,11 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from '../modules/home/home.component';
+import {ContactComponent} from '../modules/contact/contact.component';
+import {AboutUsComponent} from '../modules/about-us/about-us.component';
+import {PagesComponent} from '../modules/pages/pages.component';
+import {BlogComponent} from '../modules/blog/blog.component';
+import {LookbookComponent} from '../modules/lookbook/lookbook.component';
+import {AccessoriesComponent} from '../modules/accessories/accessories.component';
 
 
 const routes: Routes = [
             { path: 'home', component: HomeComponent},
-            { path: '', redirectTo: '/home', pathMatch: 'full' }
+            { path: '', redirectTo: '/home', pathMatch: 'full'},
+            { path: 'accessories', component: AccessoriesComponent},
+            { path: 'lookbook', component: LookbookComponent},
+            { path: 'blog', component: BlogComponent},
+            { path: 'pages', component: PagesComponent},
+            { path: 'about-us', component: AboutUsComponent},
+            { path: 'contact', component: ContactComponent},
 
 ];
 
