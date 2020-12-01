a1f7941b94444d5c714e803888fc4133b59893da
randall.erasmus <randallerasmus1@gmail.com>
2020-12-01 02:54:35 +0200

    Layout and structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './SharedComponents/header/header.component';
+import { FooterComponent } from './SharedComponents/footer/footer.component';
+import { RoutingModule } from './routing/routing.module';
+import { RouterModule} from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule} from '@angular/material/sidenav';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RoutingModule,
+    RouterModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
